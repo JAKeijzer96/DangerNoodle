57b35547f83aa5f000e4ee39fec613a24e1e63c5
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-06-25 15:42:13 +0200

    tutorial 13 - Fixing the hardcoding

--- tutorial.py
+++ tutorial.py
@@ -6,19 +6,23 @@ pygame.init() # initialize pygame modules. Returns a tuple of (succesful, unsucc
 white = (255,255,255) # RGB value of the color
 black = (0,0,0)
 red = (255,0,0)
+display_width = 800
+display_height = 600
 
-gameDisplay = pygame.display.set_mode((800,600)) # returns a surface object with (w,h) wxh pixels
+gameDisplay = pygame.display.set_mode((display_width,display_height)) # returns a surface object with (w,h) wxh pixels
 pygame.display.set_caption('Slither')
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_width//2
+lead_y = display_height//2
 lead_x_change = 0
 lead_y_change = 0
 
 clock = pygame.time.Clock() # pygame clock object used to set fps
+fps = 10
 
+block_size = 10
 
 while not gameExit:
     for event in pygame.event.get(): # gets all events (mouse movenent, key press/release, quit etc)
@@ -26,29 +30,29 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -10
+                lead_x_change = -block_size
                 lead_y_change = 0
             if event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+                lead_x_change = block_size
                 lead_y_change = 0
             if event.key == pygame.K_UP:
-                lead_y_change = -10
+                lead_y_change = -block_size
                 lead_x_change = 0
             if event.key == pygame.K_DOWN:
-                lead_y_change = 10
+                lead_y_change = block_size
                 lead_x_change = 0
 
-    if lead_x >= 800 or lead_x < 0 or lead_y >= 600 or lead_y < 0: # add boundaries
+    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0: # add boundaries
         gameExit = True
     
     lead_x += lead_x_change
     lead_y += lead_y_change
 
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10]) # parameters: surface, color, [x,y,width,height]
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size]) # parameters: surface, color, [x,y,width,height]
     pygame.display.update() # update the display
 
-    clock.tick(10) # tick(x) for a game of x frames per second
+    clock.tick(fps) # tick(x) for a game of x frames per second
 
 pygame.quit() # uninitializes everything
 quit() # quit the python program
