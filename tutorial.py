d550eb1a695285ad8ab640093e6b8a865b1020ff
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-06-25 16:37:29 +0200

    tutorial 15 - Game over functionality

--- tutorial.py
+++ tutorial.py
@@ -7,18 +7,14 @@ pygame.init() # initialize pygame modules. Returns a tuple of (succesful, unsucc
 white = (255,255,255) # RGB value of the color
 black = (0,0,0)
 red = (255,0,0)
+
 display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height)) # returns a surface object with (w,h) wxh pixels
 pygame.display.set_caption('Slither')
 
-gameExit = False
 
-lead_x = display_width//2
-lead_y = display_height//2
-lead_x_change = 0
-lead_y_change = 0
 
 clock = pygame.time.Clock() # pygame clock object used to set fps
 fps = 15
@@ -31,38 +27,61 @@ def message_to_screen(msg, color):
     screen_text = font.render(msg, True, color) # render message, True (for anti-aliasing), color
     gameDisplay.blit(screen_text, [display_width//2, display_height//2]) # show screen_text on [coords]
 
-while not gameExit:
-    for event in pygame.event.get(): # gets all events (mouse movenent, key press/release, quit etc)
-        if event.type == pygame.QUIT:
-            gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change = -block_size
-                lead_y_change = 0
-            if event.key == pygame.K_RIGHT:
-                lead_x_change = block_size
-                lead_y_change = 0
-            if event.key == pygame.K_UP:
-                lead_y_change = -block_size
-                lead_x_change = 0
-            if event.key == pygame.K_DOWN:
-                lead_y_change = block_size
-                lead_x_change = 0
+def gameLoop():
+    gameExit = False
+    gameOver = False
 
-    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0: # add boundaries
-        gameExit = True
-    
-    lead_x += lead_x_change
-    lead_y += lead_y_change
+    lead_x = display_width//2
+    lead_y = display_height//2
+    lead_x_change = 0
+    lead_y_change = 0
+    while not gameExit:
 
-    gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size]) # parameters: surface, color, [x,y,width,height]
-    pygame.display.update() # update the display
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game over.\nPress C to play again or Q to quit", red)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
 
-    clock.tick(fps) # tick(x) for a game of x frames per second
+        for event in pygame.event.get(): # gets all events (mouse movenent, key press/release, quit etc)
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                if event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                if event.key == pygame.K_UP:
+                    lead_y_change = -block_size
+                    lead_x_change = 0
+                if event.key == pygame.K_DOWN:
+                    lead_y_change = block_size
+                    lead_x_change = 0
 
-message_to_screen("You lose", red)
-pygame.display.update()
-time.sleep(2)
-pygame.quit() # uninitializes everything
-quit() # quit the python program
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0: # add boundaries
+            gameOver = True
+        
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size]) # parameters: surface, color, [x,y,width,height]
+        pygame.display.update() # update the display
+
+        clock.tick(fps) # tick(x) for a game of x frames per second
+
+    message_to_screen("You lose", red)
+    pygame.display.update()
+    time.sleep(2)
+    pygame.quit() # uninitializes everything
+    quit() # quit the python program
+
+gameLoop()
