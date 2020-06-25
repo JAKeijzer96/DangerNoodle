53be2cc888cc8bed4a76944d121053b98caba2d5
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-06-25 14:18:36 +0200

    Tutorial 5

--- tutorial.py
+++ tutorial.py
@@ -3,10 +3,13 @@ import pygame
 
 pygame.init() # initialize pygame modules. Returns a tuple of (succesful, unsuccesful) initializations
 
-gameDisplay = pygame.display.set_mode((800,600)) # tuple of height x width pixels
-pygame.display.set_caption('Slither')
+white = (255,255,255) # RGB value of the color
+black = (0,0,0)
+red = (255,0,0)
 
-pygame.display.update()
+
+gameDisplay = pygame.display.set_mode((800,600)) # returns a surface object with (h,w) hxw pixels
+pygame.display.set_caption('Slither')
 
 gameExit = False
 
@@ -15,7 +18,8 @@ while not gameExit:
         if event.type == pygame.QUIT:
             gameExit = True
         #print(event)
-
+    gameDisplay.fill(white)
+    pygame.display.update() # update the display
 
 pygame.quit() # uninitializes everything
 quit() # quit the python program
