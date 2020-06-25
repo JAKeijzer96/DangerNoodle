3e2f4e5b468a3f313cb73f5d777a271218d82b54
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-06-25 15:16:41 +0200

    tutorial 9

--- tutorial.py
+++ tutorial.py
@@ -7,7 +7,6 @@ white = (255,255,255) # RGB value of the color
 black = (0,0,0)
 red = (255,0,0)
 
-
 gameDisplay = pygame.display.set_mode((800,600)) # returns a surface object with (w,h) wxh pixels
 pygame.display.set_caption('Slither')
 
@@ -18,6 +17,9 @@ lead_y = 300
 lead_x_change = 0
 lead_y_change = 0
 
+clock = pygame.time.Clock() # pygame clock object used to set fps
+
+
 while not gameExit:
     for event in pygame.event.get(): # gets all events (mouse movenent, key press/release, quit etc)
         if event.type == pygame.QUIT:
@@ -25,12 +27,16 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
+                lead_y_change = 0
             if event.key == pygame.K_RIGHT:
                 lead_x_change = 10
+                lead_y_change = 0
             if event.key == pygame.K_UP:
                 lead_y_change = -10
+                lead_x_change = 0
             if event.key == pygame.K_DOWN:
                 lead_y_change = 10
+                lead_x_change = 0
     
     lead_x += lead_x_change
     lead_y += lead_y_change
@@ -38,6 +44,8 @@ while not gameExit:
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10]) # parameters: surface, color, [x,y,width,height]
     pygame.display.update() # update the display
+
+    clock.tick(10) # tick(x) for a game of x frames per second
 
 pygame.quit() # uninitializes everything
 quit() # quit the python program
