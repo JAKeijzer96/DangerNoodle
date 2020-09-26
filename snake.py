83f1a4d00d6d8021cd7da7161df01ad6a8b570db
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-09-26 18:39:45 +0200

    BIG update - changed offscreen handling to work with the modulo operator (%) instead of resetting coordinates with x=0 or x=display_width - block_size type of assignments. This allows tail to stay properly rotated when the body segment before it has already appeared on the other side of the screen. Also added very minor shutdown() function

--- snake.py
+++ snake.py
@@ -29,7 +29,6 @@ FPS = 15 # TODO: Find optimal fps with possibly lower x,y increments per clock t
 BLOCK_SIZE = 20
 
 # TODO: Make proper start menu, with options and local highscore submenus, arrow key movement, possible mouse support
-# TODO: Add dark mode option
 # TODO: Add toggle for boundaries
 # TODO: Add local highscore file with related function
 # TODO: Add different maps with obstacles?
@@ -53,11 +52,12 @@ class Snake():
         self.smallfont = pg.font.Font(REGULAR_FONT, 25) # TODO: find prettier font and matching sizes
         self.medfont = pg.font.Font(REGULAR_FONT, 40) # size 50
         self.largefont = pg.font.Font(REGULAR_FONT, 80) # size 80
-        # Initialize colors
+        # Initialize options, changeable in options menu
         self.background_color = WHITE
         self.text_color_normal = BLACK
         self.text_color_emphasis = RED
         self.snake_color = SNAKE_GREEN
+        self.boundaries = True
         # Call function to reset game variables, in this case initializing them
         self.reset_game_variables()
 
@@ -77,6 +77,10 @@ class Snake():
         #self.tail = self.tail_img
         self.score = 0 # score, could use snake_length - base length but base length still not certain # TODO ?
         self.rand_apple_x, self.rand_apple_y = self.rand_apple_gen() # TODO: refactor? also comment functionality
+    
+    def shutdown(self):
+        pg.quit()
+        exit()
 
     def draw_main_menu(self):
         # Print introductory messages (start menu)
@@ -95,15 +99,13 @@ class Snake():
         while not self.program_exit:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.shutdown()
                 elif event.type == pg.KEYDOWN:
                     if event.key == pg.K_c:
                         self.game_loop()
                         self.draw_main_menu()
                     elif event.key == pg.K_q:
-                        pg.quit()
-                        exit()
+                        self.shutdown()
                     elif event.key == pg.K_o:
                         self.options_menu()
                         self.draw_main_menu()
@@ -119,7 +121,8 @@ class Snake():
         self.program_surface.fill(self.background_color)
         self.message_to_screen("Temporary Options menu", self.text_color_normal, -100, "large")
         self.message_to_screen("Press D to toggle dark mode", self.text_color_normal)
-        self.message_to_screen("Press BACKSPACE to return to main menu", self.text_color_normal, 50)
+        self.message_to_screen(f"Press B to toggle edge boundaries. Currently: {self.boundaries}", self.text_color_normal, 50)
+        self.message_to_screen("Press BACKSPACE to return to main menu", self.text_color_normal, 250)
         pg.display.update()
 
     def options_menu(self):
@@ -131,12 +134,14 @@ class Snake():
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_BACKSPACE:
                         in_submenu = False
-                    if event.key == pg.K_d:
+                    elif event.key == pg.K_d:
                         self.toggle_dark_mode()
                         self.draw_options_menu()
+                    elif event.key == pg.K_b:
+                        self.boundaries = not self.boundaries
+                        self.draw_options_menu()
                 elif event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.shutdown()
             
             # TODO: find optimal clock tick here
             self.clock.tick(15)
@@ -171,7 +176,7 @@ class Snake():
     def pause(self):
         # Update once when paused, then only check for event handling
         self.message_to_screen("Paused", self.text_color_normal, -100, size="large")
-        self.message_to_screen("Press C to continue or Q to quit", self.text_color_normal)
+        self.message_to_screen("Press C to continue, Q to quit or BACKSPACE to return to the main menu", self.text_color_normal)
         pg.display.update()
         
         paused = True
@@ -180,41 +185,64 @@ class Snake():
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_c:
                         paused = False
+                    elif event.key == pg.K_BACKSPACE:
+                        self.reset_game_variables()
+                        paused = False
                     elif event.key == pg.K_q:
-                        pg.quit()
-                        exit()
+                        self.shutdown()
                 elif event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.shutdown()
             # TODO: find optimal fps here
             self.clock.tick(15) # dont need high fps
 
     def rotate(self, img, degrees):
         return pg.transform.rotate(img, degrees)
 
-    def snake(self, block_size, snake_list):
+    def snake(self, snake_list):
         draw_tail = 0 # used in list slicing to determine whether or not to draw the tail without raising index errors
 
-        self.program_surface.blit(self.head, (self.snake_list[-1][0], self.snake_list[-1][1])) # blit the snake head image
+        self.program_surface.blit(self.head, (self.snake_list[-1][0] % DISPLAY_WIDTH, self.snake_list[-1][1] % DISPLAY_HEIGHT)) # blit the snake head image
         
-        if len(self.snake_list) > 1:
+        if len(self.snake_list) > 1: # TODO: code snake to appear fully on game start instead of being generated from the spawning point
+                                     # This would remove the need for checking if len(snake_list) > 1
+                                     # Currently snake_list is always > 1, except for the very first frame of the game
             draw_tail = 1
-            if self.snake_list[1][0] < self.snake_list[0][0]: # x less, so go left
-                tail = self.rotate(self.tail_img, 90)
-            elif self.snake_list[1][1] > self.snake_list[0][1]: # y greater, go down
-                tail = self.rotate(self.tail_img, 180)
-            elif self.snake_list[1][0] > self.snake_list[0][0]: # x greater, go right
-                tail = self.rotate(self.tail_img, 270)
-            else: # going up
-                tail = self.tail_img
+            tail_x, tail_y = self.snake_list[0][0], self.snake_list[0][1]
+            pretail_x, pretail_y = self.snake_list[1][0], self.snake_list[1][1]
+            if pretail_x < tail_x:
+                # pretail_x is less than tail_x, so the snake is moving left
+                # Exception: When self.boundaries == False AND we're moving right AND pretail has just appeared
+                # on the far left of the screen. In that case we still want the tail to be pictured as heading right
+                # Similar logic applies for the other directions
+                if not self.boundaries and pretail_x >= DISPLAY_WIDTH and pretail_x % DISPLAY_WIDTH == 0:
+                    tail = self.rotate(self.tail_img, 270)
+                else:
+                    tail = self.rotate(self.tail_img, 90)
+            elif pretail_y > tail_y: # y greater, going down
+                if not self.boundaries and pretail_y < 0 and pretail_y % DISPLAY_HEIGHT == DISPLAY_HEIGHT:# - BLOCK_SIZE:
+                    tail = self.tail_img
+                else:
+                    tail = self.rotate(self.tail_img, 180)
+            elif pretail_x > tail_x: # x greater, going right
+                if not self.boundaries and pretail_x < 0 and pretail_x % 480 == DISPLAY_WIDTH:# - BLOCK_SIZE:
+                    tail = self.rotate(self.tail_img, 90)
+                else:
+                    tail = self.rotate(self.tail_img, 270)
+            elif pretail_y < tail_y: # y less, going up
+                if not self.boundaries and pretail_y >= DISPLAY_HEIGHT and pretail_y % DISPLAY_HEIGHT == 0:
+                    tail = self.rotate(self.tail_img, 180)
+                else:
+                    tail = self.tail_img
 
-            self.program_surface.blit(tail, (self.snake_list[0][0], self.snake_list[0][1]))
+            self.program_surface.blit(tail, (tail_x % DISPLAY_WIDTH, tail_y % DISPLAY_HEIGHT))
         
         # TODO: Add extra segment images for the body and when we turn a corner
         # TODO: For corner image, check coords before and after current segment to see which way to rotate the image
 
+        # Modulo divisor (%) in coords for when self.boundaries == False
         for x_and_y in self.snake_list[draw_tail:-1]: # the last element is the head, so dont put a square there
-            pg.draw.rect(self.program_surface, self.snake_color, [x_and_y[0],x_and_y[1],BLOCK_SIZE,BLOCK_SIZE]) # parameters: surface, color, [x,y,width,height]
+            pg.draw.rect(self.program_surface, self.snake_color,
+                    [x_and_y[0] % DISPLAY_WIDTH, x_and_y[1] % DISPLAY_HEIGHT, BLOCK_SIZE,BLOCK_SIZE]) # parameters: surface, color, [x,y,width,height]
 
     def text_objects(self, text, color, size):
         if size == "small":
@@ -234,15 +262,15 @@ class Snake():
     def game_over_function(self):
         # Only paste text once
         self.message_to_screen("Game over", self.text_color_emphasis, y_displace=-50, size="large")
-        self.message_to_screen("Press C to play again, Q to quit or BACKSPACE to return to main menu", self.text_color_normal, 50, size="med")
+        self.message_to_screen("Press C to play again, Q to quit", self.text_color_normal, 50, size="med")
+        self.message_to_screen("or BACKSPACE to return to main menu", self.text_color_normal, 100, size="med")
         pg.display.update()
         
         while self.game_over:
             for event in pg.event.get():
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_q:
-                        pg.quit()
-                        exit()
+                        self.shutdown()
                     elif event.key == pg.K_c:
                         # Reset variables, then reset in_game to True to return to game_loop()
                         self.reset_game_variables()
@@ -252,8 +280,7 @@ class Snake():
                         # and return to main_menu()
                         self.reset_game_variables()
                 elif event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.shutdown()
 
     def game_loop(self):
         self.in_game = True
@@ -287,22 +314,29 @@ class Snake():
                     elif event.key == pg.K_p:
                         self.pause()
                 elif event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.shutdown()
 
-            if self.lead_x >= DISPLAY_WIDTH or self.lead_x < 0 or self.lead_y >= DISPLAY_HEIGHT or self.lead_y < 0: # add boundaries
-                self.game_over = True # TODO: comment on if statement
-            
             self.lead_x += self.lead_x_change
             self.lead_y += self.lead_y_change
+
+            # Add boundaries to the game. If x or y is outside the game window, game over
+            if self.boundaries and (self.lead_x >= DISPLAY_WIDTH or self.lead_x < 0 or self.lead_y >= DISPLAY_HEIGHT or self.lead_y < 0):
+                self.game_over = True
+                # elif not self.boundaries:
+                #     if self.lead_x >= DISPLAY_WIDTH:
+                #         self.lead_x = 0
+                #     elif self.lead_x < 0:
+                #         self.lead_x = DISPLAY_WIDTH - BLOCK_SIZE
+                #     elif self.lead_y >= DISPLAY_HEIGHT:
+                #         self.lead_y = 0
+                #     elif self.lead_y < 0:
+                #         self.lead_y = DISPLAY_HEIGHT-BLOCK_SIZE
 
             self.program_surface.fill(self.background_color)
             self.program_surface.blit(self.apple_img, (self.rand_apple_x, self.rand_apple_y))
             
             # TODO: check if this is needed or double functionality
-            snake_head = []
-            snake_head.append(self.lead_x)
-            snake_head.append(self.lead_y)
+            snake_head = [self.lead_x, self.lead_y]
             self.snake_list.append(snake_head)
             
             if len(self.snake_list) >  self.snake_length:
@@ -313,11 +347,12 @@ class Snake():
                 if segment == snake_head:
                     self.game_over = True
 
-            self.snake(BLOCK_SIZE, self.snake_list)
+            self.snake(self.snake_list)
             self.print_score(self.score)
 
             # Collision checking for grid-based and same-size apple/snake
-            if self.lead_x == self.rand_apple_x and self.lead_y == self.rand_apple_y:
+            # Modulo divisor (%) for when boundaries are disabled
+            if [self.lead_x % DISPLAY_WIDTH, self.lead_y % DISPLAY_HEIGHT] == [self.rand_apple_x, self.rand_apple_y]:
                 self.rand_apple_x, self.rand_apple_y = self.rand_apple_gen()
                 self.snake_length += 1
                 self.score += 1
@@ -339,4 +374,3 @@ class Snake():
 if __name__ == "__main__":
     snek = Snake()
     snek.main_menu()
-    snek.game_loop()
